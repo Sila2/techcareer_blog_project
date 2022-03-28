52a53565a23faec1e261389698eb202748a831ef
Sila2 <silaikinci@hotmail.com>
2022-03-28 20:08:22 +0300

    jquery

---
+++ _06_jquery/jquery.js
@@ -0,0 +1,17 @@
+/*
+JQUERY
+2006 John Resig geliştiriyor
+JavaScript kütüphanesidir.
+Programlama dili değildir.
+Az kod çok iş mantığına sahiptir.
+Ajax yazmak kolaydır.
+DOM manipülasyonları yazma maliyeti daha düşüktür.
+*/
+
+$(function() {
+    // JS DOM
+    //document.getElementById("jquery_id").innerHTML="<b>jquery buradaydı</b>";
+
+    // JQUERY
+    $("#jquery_id").html("<b>jquery buradaydı</b>")
+})
