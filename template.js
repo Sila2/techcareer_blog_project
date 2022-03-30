151c6d4bd3ea766c8474f9bd38b1551898b565f5
Sila2 <silaikinci@hotmail.com>
2022-03-30 18:51:49 +0300

    .

--- template.js
+++ template.js
@@ -1,3 +1,19 @@
+//modal active
 $(function() {
+    $("#btn_giris").click(function() {
+        $("#myUye").modal("show");
+    })
+
+})
+
+//form validation
+$(function() {
+    $("#btnSubmit").click(function() {
+        //input içeriği aldım
+        let email, password;
+        email = $("#user_email").val();
+        password = $("#user_password").val();
+        alert(email + " " + password)
+    })
 
 })
