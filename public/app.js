ab60152c607ef0816b58ade2867516f76d8a7222
Sila2 <silaikinci@hotmail.com>
2022-04-29 01:35:52 +0300

    live-server and babel success

---
+++ public/app.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var template = React.createElement("h3", null, "babel olmadan yazıldı");
+var root = document.getElementById("root");
+ReactDOM.render(template, root);
---
+++ src/app.js
@@ -0,0 +1,4 @@
+var template = React.createElement("h3", null, "babel olmadan yazıldı");
+
+var root = document.getElementById("root");
+ReactDOM.render(template, root);
