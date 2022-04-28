cdc5a6d56c8b509b16e83b1f7af7b8650f593f71
Sila2 <silaikinci@hotmail.com>
2022-04-29 02:04:55 +0300

    react, babel added

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 //root
-var template = React.createElement("h3", null, "babel olmadan yazıldı");
-
 var root = document.getElementById("root");
+
+//template
+var template = React.createElement("h3", { id: "h3_id" }, "babel var");
+
+//ReactDOM
 ReactDOM.render(template, root);
