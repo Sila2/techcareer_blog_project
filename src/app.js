83c593dd620e57eb53d6b5fcd7b72687ac297623
Sila2 <silaikinci@hotmail.com>
2022-04-29 14:31:44 +0300

    template literal, global scope, function scope, let,const,var

--- src/app.js
+++ src/app.js
@@ -1,8 +1,75 @@
 //root
 var root = document.getElementById("root");
 
+////////////////////////
+//Hoisting: değişkeni sona yazıyoruz.
+deneme = "Merhabalar";
+let deneme;
+
+// global scope
+// var globalScope="global değişken";
+// let globalFunction=(p1)=>{
+//     p1+=globalScope;
+//     console.log(p1)
+// }
+
+//local scope (FunctionScope)
+// let functionScope=(p1)=>{
+//     var localScope="local değişken";
+//     p1+=localScope;
+//     console.log(p1)
+// }
+// functionScope("Nasılsın");
+// console.log(localScope)
+
+///////////////
+//VAR: aynı değişken isminden yazabiliriz
+//var
+
+//LET: aynı değişken isminden yazamayız
+//let
+//const
+
+/////////////
+//Template Literal
+let number1 = "Number1";
+let templateLiteral = `template literal 
+multi-line`;
+
+let templateLiteralInterpolation = `Adım: ${number1}`;
+
+// var variable1, variable2, variable3, variable4;
+// variable1="Html5";
+// variable2="Css3";
+// variable3="JS";
+// variable4="React";
+
+var object = {
+    variable1: "Html5",
+    variable2: "Css3",
+    variable3: "JS",
+    variable4: "React",
+}
+
+//react yorumlama {}
+//react class => className
+
 //template
-var template = React.createElement("h3", { id: "h3_id" }, "babel var");
+var template =
+    ( <
+        div >
+        <
+        h1 > başlık < /h1> <
+        p className = "text-danger" > Lorem ipsum dolor sit amet. < /p> <
+        ul className = "list-group" >
+        <
+        li className = "list-group-item active" > { variable1 } < /li> <
+        li className = "list-group-item" > { variable2 } < /li> <
+        li className = "list-group-item" > { variable3 } < /li> <
+        li className = "list-group-item" > { variable4 } < /li> <
+        /ul> <
+        /div>
+    );
 
 //ReactDOM
 ReactDOM.render(template, root);
